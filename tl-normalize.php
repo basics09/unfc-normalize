7e5e4f89afd8fe7c7eea9a045e20c1af7deb0286
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2014-11-09 01:52:00 +0100

    Initial commit

---
+++ tl-normalize.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: TL Normalizer
+ * Plugin URI: http://torstenlandsiedel.de
+ * Description: Normalizes content, excerpt, title and comment content to Normalization Form C.
+ * Version: 1.0.0
+ * Author: Torsten Landsiedel
+ * Author URI: http://torstenlandsiedel.de
+ * License: GPLv2
+ */
+
+function tl_normalizer( $content ) {
+
+	/*
+	 * Why?
+	 *
+	 * For everyone getting this warning from W3C: "Text run is not in Unicode Normalization Form C."
+	 * http://www.w3.org/International/docs/charmod-norm/#choice-of-normalization-form
+	 *
+	 * Requires PHP 5.3+
+	 * Be sure to have the PHP-Normalizer-extension (intl and icu) installed.
+	 * See: http://php.net/manual/en/normalizer.normalize.php
+	 */
+	if ( ! normalizer_is_normalized( $content, Normalizer::FORM_C ) ) {
+		$content = normalizer_normalize( $content, Normalizer::FORM_C );	
+	}
+
+	return $content;
+}
+
+add_filter( 'content_save_pre', 'tl_normalizer' );
+add_filter( 'title_save_pre' , 'tl_normalizer' );
+add_filter( 'pre_comment_content' , 'tl_normalizer' );
+add_filter( 'excerpt_save_pre' , 'tl_normalizer' );
