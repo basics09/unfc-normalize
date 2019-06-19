674b138eddc2127943b788f846deff681df39874
gitlost <burmartke@gmail.com>
2019-06-19 22:05:57 +0100

    Try linking wp-config.php

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -115,6 +115,8 @@ install_test_suite() {
 	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
 	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
 	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+	# For WP-CLI
+	ln -s wp-tests-config.php wp-config.php
 }
 
 install_wp
